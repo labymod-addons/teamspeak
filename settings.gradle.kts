7bce49448437b3173c3ddb11f64f2e705ad45c8d
Christian <r0bby@gmx.net>
2026-06-03 13:58:53 +0200

    Update LabyGradle to 0.8.0, sync CI workflow

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 rootProject.name = "teamspeak"
 
 pluginManagement {
-    val labyGradlePluginVersion = "0.5.7"
+    val labyGradlePluginVersion = "0.8.0"
 
     buildscript {
         repositories {

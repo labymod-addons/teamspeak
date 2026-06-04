61655c1818e91d6f7b6175f264045afeaf7e3f2b
Christian <r0bby@gmx.net>
2026-06-04 05:46:54 +0200

    Update LabyGradle to 0.8.1

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 rootProject.name = "teamspeak"
 
 pluginManagement {
-    val labyGradlePluginVersion = "0.8.0"
+    val labyGradlePluginVersion = "0.8.1"
 
     buildscript {
         repositories {
